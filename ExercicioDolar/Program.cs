af75872429a41f36023b61c40576c45f164e3535
Jorge Esteves <jhsesteves@gmail.com>
2020-05-01 10:30:33 -0300

    Projeto iniciado, calculando a conversão de real para dollar

---
+++ ExercicioDolar/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioDolar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Qual é a cotação do dólar? R$ ");
+            double cotacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Quantos dólares você vai comprar? $ ");
+            double quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double result = ConversorDeMoedas.DolarParaReal(quantia, cotacao);
+            Console.WriteLine("Valor a ser pago em reais = R$ " + result.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
